06f1415c293260ca5c125d769930e567e997fba1
emanuel <sswemanuel@gmail.com>
2026-06-13 23:12:04 -0600

    feat: add tailwind method to bootstrapt tailwind library

--- vite.config.js
+++ vite.config.js
@@ -5,6 +5,7 @@ import tailwindcss from '@tailwindcss/vite';
 
 export default defineConfig({
     plugins: [
+        tailwindcss(),
         laravel({
             input: ['resources/css/app.css', 'resources/js/app.js'],
             refresh: true,
