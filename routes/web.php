ec752dad0184b66c5ca2360d3f85af294e1fb7c6
emanuel <sswemanuel@gmail.com>
2026-06-13 23:02:15 -0600

    feat: add default route for website

--- routes/web.php
+++ routes/web.php
@@ -1,1 +1,9 @@
 <?php
+
+use App\Http\Controllers\HomeController;
+use Illuminate\Support\Facades\Route;
+
+/**
+ * Define default route for website
+ */
+Route::get('/', [HomeController::class, 'index']);
